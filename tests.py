7e359c0e4df083b58890c816eb4b8edae37bac13
Costas Basdekis <costas.basdekis@gmail.com>
2016-08-16 07:46:45 +0100

    Add more tests

--- tests.py
+++ tests.py
@@ -11,6 +11,32 @@ class TestSquareBoard(base_tests.BaseAllBoardTests):
     minimum_cell_side_count = 4
     maximum_cell_side_count = 4
 
+    def test_cells_in_row_are_neighbours(self):
+        cells_that_are_not_neighbours_in_row = {
+            cell
+            for y in xrange(self.puzzle.height)
+            for cell, next_cell in (
+                (self.puzzle.cells_by_key[(x, y)],
+                 self.puzzle.cells_by_key[(x + 1, y)])
+                for x in xrange(self.puzzle.width - 1)
+            )
+            if not cell in next_cell.neighbours
+        }
+        self.assertFalse(cells_that_are_not_neighbours_in_row)
+
+    def test_cells_in_column_are_neighbours(self):
+        cells_that_are_not_neighbours_in_column = {
+            cell
+            for x in xrange(self.puzzle.width)
+            for cell, next_cell in (
+                (self.puzzle.cells_by_key[(x, y)],
+                 self.puzzle.cells_by_key[(x, y + 1)])
+                for y in xrange(self.puzzle.height - 1)
+            )
+            if not cell in next_cell.neighbours
+        }
+        self.assertFalse(cells_that_are_not_neighbours_in_column)
+
 
 class TestSquarePuzzle(base_tests.BaseAllPuzzleTests):
     puzzle_class = square_puzzle.SquarePuzzle
@@ -26,6 +52,45 @@ class TestHexagonalBoard(base_tests.BaseAllBoardTests):
     puzzle_kwargs = {'width': 20, 'height': 20}
     minimum_cell_side_count = 6
     maximum_cell_side_count = 6
+
+    def test_cells_in_row_are_neighbours(self):
+        cells_that_are_not_neighbours_in_row = {
+            cell
+            for y in xrange(self.puzzle.height)
+            for cell, next_cell in (
+                (self.puzzle.cells_by_key[(x, y)],
+                 self.puzzle.cells_by_key[(x + 1, y)])
+                for x in xrange(self.puzzle.width - 1)
+            )
+            if not cell in next_cell.neighbours
+        }
+        self.assertFalse(cells_that_are_not_neighbours_in_row)
+
+    def test_cells_in_column_right_are_neighbours(self):
+        cells_that_are_not_neighbours_in_column = {
+            cell
+            for x in xrange(self.puzzle.width)
+            for cell, next_cell in (
+                (self.puzzle.cells_by_key[(x, y)],
+                 self.puzzle.cells_by_key[(x, y + 1)])
+                for y in xrange(self.puzzle.height - 1)
+            )
+            if not cell in next_cell.neighbours
+        }
+        self.assertFalse(cells_that_are_not_neighbours_in_column)
+
+    def test_cells_in_column_left_are_neighbours(self):
+        cells_that_are_not_neighbours_in_column = {
+            cell
+            for x in xrange(1, self.puzzle.width)
+            for cell, next_cell in (
+                (self.puzzle.cells_by_key[(x, y)],
+                 self.puzzle.cells_by_key[(x - (1 if (y % 2 == 0) else 0), y + 1)])
+                for y in xrange(self.puzzle.height - 1)
+            )
+            if not cell in next_cell.neighbours
+        }
+        self.assertFalse(cells_that_are_not_neighbours_in_column)
 
 
 class TestHexagonalPuzzle(base_tests.BaseAllPuzzleTests):
