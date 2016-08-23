8338b805cb42d1e40380e4c753f7fad6aa0b9b05
Costas Basdekis <costas.basdekis@gmail.com>
2016-08-23 08:33:38 +0100

    Create solved SVG in tests

--- slithering/tests/base_tests.py
+++ slithering/tests/base_tests.py
@@ -447,12 +447,23 @@ class BaseTestPuzzleCreation(BaseTestPuzzle):
 
 
 class BaseTestPuzzleSVG(BaseTestPuzzle):
+    solver_class = puzzle_solver.PuzzleSolver
+
     def test_can_create_svg(self):
         svg = self.puzzle.create_svg(**self.puzzle_svg_kwargs)
         print svg.filename
 
     def test_can_create_unsolved_svg(self):
         svg = self.puzzle.create_unsolved_svg(**self.puzzle_svg_kwargs)
+        print svg.filename
+
+    def test_can_create_solved_svg(self):
+        solver = self.solver_class(self.puzzle)
+        solver.solve()
+        puzzle_svg_kwargs = dict(self.puzzle_svg_kwargs)
+        filename = '/tmp/%s_solved.svg' % type(self.puzzle).__name__
+        puzzle_svg_kwargs.setdefault('filename', filename)
+        svg = self.puzzle.create_unsolved_svg(**puzzle_svg_kwargs)
         print svg.filename
 
 
